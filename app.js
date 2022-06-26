dbf15f57f77fd8e1831caddd3f076a6f41eb3678
S-Y <sy-unimportant@outlook.com>
2022-06-26 11:53:13 +0100

    completed 46. Router basics set up

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 
+const router = require('./router.js');
+
 app.use(express.static('public')); // static is for css. the css files are in the public folder
 app.set('views', 'views'); // first views is an express method, second is the name of the folder containing HTML.
 app.set('view engine', 'ejs'); // setting a view engine for templates. Ejs is the engine.
 
-app.get('/', function (req, res) {
-    res.render('home-guest');
-});
+app.use('/', router);
 
 app.listen(3000);
